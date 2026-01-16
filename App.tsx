d99bb127baf0830b16a0d71b376ec9169e002311
Ahmad Sadiq <ahmadsadiq6278@gmail.com>
2026-01-16 10:03:08 -0500

    first commit

--- App.tsx
+++ App.tsx
@@ -1,13 +1,137 @@
+// import { AppleMaps, GoogleMaps } from 'expo-maps';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform, Button } from 'react-native';
+import * as Location from 'expo-location';
+import { useEffect, useState } from 'react';
+import { LocationSubscriber } from 'expo-location/build/LocationSubscribers';
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+import Constants from 'expo-constants';
+import { AppleMaps } from 'expo-maps';
+import * as AgeRange from 'expo-age-range';
 
 export default function App() {
+  const [location, setLocation] = useState<Location.LocationObject | null>(null)
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [channels, setChannels] = useState<Notifications.NotificationChannel[]>([]);
+  const [notification, setNotification] = useState<Notifications.Notification | undefined>(
+    undefined
+  );
+  const [result, setResult] = useState(null);
+
+  const requestAgeRange = async () => {
+    try {
+      const ageRange = await AgeRange.requestAgeRangeAsync({
+        threshold1: 10,
+        threshold2: 13,
+        threshold3: 18,
+      });
+      setResult(ageRange);
+    } catch (error) {
+      console.log(error);
+      setResult({ error: error.message });
+    }
+  }
+
+  const getUserLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      alert('Permission to access location was denied');
+      return;
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    setLocation(location);
+  }
+
+  useEffect(() => {
+    getUserLocation()
+
+  }, [])
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then(token => token && setExpoPushToken(token));
+
+    if (Platform.OS === 'android') {
+      Notifications.getNotificationChannelsAsync().then(value => setChannels(value ?? []));
+    }
+    const notificationListener = Notifications.addNotificationReceivedListener(notification => {
+      console.log("Notification", notification)
+      setNotification(notification);
+    });
+
+    const responseListener = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      notificationListener.remove();
+      responseListener.remove();
+    };
+  }, []);
+
+  async function registerForPushNotificationsAsync() {
+    let token;
+
+    if (Platform.OS === 'android') {
+      await Notifications.setNotificationChannelAsync('myNotificationChannel', {
+        name: 'A channel is needed for the permissions prompt to appear',
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: '#FF231F7C',
+      });
+    }
+
+    if (Device.isDevice) {
+      const { status: existingStatus } = await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== 'granted') {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== 'granted') {
+        alert('Failed to get push token for push notification!');
+        return;
+      }
+      // Learn more about projectId:
+      // https://docs.expo.dev/push-notifications/push-notifications-setup/#configure-projectid
+      // EAS projectId is used here.
+      try {
+        const projectId =
+          Constants?.expoConfig?.extra?.eas?.projectId ?? Constants?.easConfig?.projectId;
+        if (!projectId) {
+          throw new Error('Project ID not found');
+        }
+        token = (
+          await Notifications.getExpoPushTokenAsync({
+            projectId,
+          })
+        ).data;
+        console.log(token);
+      } catch (e) {
+        token = `${e}`;
+      }
+    } else {
+      alert('Must use physical device for Push Notifications');
+    }
+
+    return token;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Location</Text>
+      <Text>Lat:{location?.coords.latitude}</Text>
+      <Text>Lat:{location?.coords.longitude}</Text>
+      <Button title="Request Age Range" onPress={requestAgeRange} />
+      {result && (
+        <Text style={styles.result}>
+          {'error' in result ? `Error: ${result.error}` : `Lower age bound: ${result.lowerBound}`}
+        </Text>
+      )}
     </View>
-  );
+  )
+
 }
 
 const styles = StyleSheet.create({
